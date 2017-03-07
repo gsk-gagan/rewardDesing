59a5566ac0f3dab20cbbc076528ad8c09ad1dc05
Gagandeep S. Kaler <gagandeep.singh.iitdelhi@gmail.com>
2017-03-08 00:05:57 +0530

    Working Navigation Drawer

--- sample/src/main/java/com/github/florent37/materialviewpager/sample/MainActivity.java
+++ sample/src/main/java/com/github/florent37/materialviewpager/sample/MainActivity.java
@@ -1,9 +1,12 @@
 package com.github.florent37.materialviewpager.sample;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -23,7 +26,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends DrawerActivity {
+public class MainActivity extends DrawerActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     @BindView(R.id.materialViewPager)
     MaterialViewPager mViewPager;
@@ -118,5 +122,29 @@ public class MainActivity extends DrawerActivity {
                 }
             });
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
